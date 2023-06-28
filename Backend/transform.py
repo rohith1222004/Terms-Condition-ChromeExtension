2146a9328feeb7969ba83f145a528c852b40479c
Sasidharan <dharan731@gmail.com>
2023-06-28 10:01:04 +0530

    format data

--- Backend/transform.py
+++ Backend/transform.py
@@ -1,12 +1,28 @@
-import json 
+# import json
+# f = open("./TermsAndConditions.jsonl", encoding='utf8')
+# f2 = open("./data.jsonl", 'w+', encoding='utf8')
+# for x in f:
+#     data = json.loads(x)
+#     string = '{{"prompt":"{sent}","completion":"{lev}"}}\n'
+
+#     f2.write(string.format(
+#         sent=data['sentence'], lev=data['unfairness_level']))
+#     print(data['sentence'])
+
+
+import json
+
 f = open("./TermsAndConditions.jsonl", encoding='utf8')
 f2 = open("./data.jsonl", 'w+', encoding='utf8')
-for x in f:
-  data = json.loads(x)
-  string = '{{"prompt" : "{sent}", "completion" : "{lev}"}} \n'
-#   f2.write(
-#     "{" + '"prompt":' + '"' + data['sentence'] + '"' + " + ","} \n"
-#     )
 
-  f2.write(string.format(sent = data['sentence'], lev = data['unfairness_level']))
-  print(data['sentence'])
+for x in f:
+    data = json.loads(x)
+    prompt = json.dumps(data['sentence'])
+    completion = json.dumps(data['unfairness_level'])
+    string = '{{"prompt":{sent},"completion":{lev}}}\n'
+
+    f2.write(string.format(sent=prompt, lev=completion))
+    print(data['sentence'])
+
+f.close()
+f2.close()
